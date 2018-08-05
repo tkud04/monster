07708d018cfedad62db835b74c6392b7263a2fbd
ANonymous <postmaster.richmama@gmail.com>
2018-08-05 22:41:35 +0100

    push na

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -4,15 +4,5 @@ namespace App\Helpers\Contracts;
 Interface HelperContract
 {
         public function sendEmail($to,$subject,$data,$view,$type);
-        public function addRecord($data);
-        public function addPayment($data);
-        public function addReferral($data);
-        public function deleteRecords($gg);
-        public function deletePayment($gg);
-        public function getRecords();
-        public function getPayments();
-        public function getRecord($gg);
-        public function markPayment($data);
-        public function getPaymentStatus($gg);
 }
  ?>
